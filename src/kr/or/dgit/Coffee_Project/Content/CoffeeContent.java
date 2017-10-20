be8267f479d37f842ec7f8c9e131553f481cb297
Mosquito(A) <skykim10908@naver.com>
2017-10-20 09:44:30 +0900

    자동 제품명 표기 구현

--- src/kr/or/dgit/Coffee_Project/Content/CoffeeContent.java
+++ src/kr/or/dgit/Coffee_Project/Content/CoffeeContent.java
@@ -1,6 +1,8 @@
 package kr.or.dgit.Coffee_Project.Content;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
@@ -16,30 +18,35 @@ public class CoffeeContent extends JPanel {
 	private TextFieldComponent panelPtotal;
 	private TextFieldComponent panelPmargin;
 
-	/**
-	 * Create the panel.
-	 */
+	public TextFieldComponent getPanelPcode() {
+		return panelPcode;
+	}
+
+	public TextFieldComponent getPanelPname() {
+		return panelPname;
+	}
+
 	public CoffeeContent() {
 		setLayout(new GridLayout(0, 1, 0, 10));
-		
+
 		panelPcode = new TextFieldComponent("제품코드");
 		add(panelPcode);
-		
+
 		panelPname = new TextFieldComponent("제품명");
 		add(panelPname);
-		
+
 		panelPprice = new TextFieldComponent("제품단가");
 		add(panelPprice);
-		
+
 		panelPtotal = new TextFieldComponent("판매수량");
 		add(panelPtotal);
-		
+
 		panelPmargin = new TextFieldComponent("마진율");
 		add(panelPmargin);
 
 	}
-	
-	public Coffee getContent(){
+
+	public Coffee getContent() {
 		Product pCode = new Product(panelPcode.getTextValue(), 0);
 		Product pName = new Product(panelPname.getTextValue(), 1);
 		int pPrice = Integer.parseInt(panelPprice.getTextValue());
@@ -47,30 +54,30 @@ public class CoffeeContent extends JPanel {
 		int pMargin = Integer.parseInt(panelPmargin.getTextValue());
 		return new Coffee(pCode, pName, pPrice, pTotal, pMargin);
 	}
-	
-	public void setContent(Coffee coffee){
+
+	public void setContent(Coffee coffee) {
 		panelPcode.setTextValue(coffee.getpCode().getpCode());
 		panelPname.setTextValue(coffee.getpName().getpName());
 		panelPprice.setTextValue(String.valueOf(coffee.getpPrice()));
 		panelPtotal.setTextValue(String.valueOf(coffee.getpTotal()));
 		panelPmargin.setTextValue(String.valueOf(coffee.getpMargin()));
-	
+
 	}
-	
-	public void isEmptyCheck() throws Exception{
+
+	public void isEmptyCheck() throws Exception {
 		panelPcode.isEmptyCheck();
 		panelPname.isEmptyCheck();
 		panelPprice.isEmptyCheck();
 		panelPtotal.isEmptyCheck();
 		panelPmargin.isEmptyCheck();
 	}
-	
-	public void clear(){
+
+	public void clear() {
 		panelPcode.setTextValue("");
 		panelPname.setTextValue("");
 		panelPprice.setTextValue("");
 		panelPtotal.setTextValue("");
 		panelPmargin.setTextValue("");
 	}
-	
+
 }
--- src/kr/or/dgit/Coffee_Project/view/CoffeeView.java
+++ src/kr/or/dgit/Coffee_Project/view/CoffeeView.java
@@ -6,6 +6,11 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -22,6 +27,7 @@ import kr.or.dgit.Coffee_Project.common.SaleListComponent;
 import kr.or.dgit.Coffee_Project.dto.Coffee;
 import kr.or.dgit.Coffee_Project.dto.Product;
 import kr.or.dgit.Coffee_Project.service.CoffeeService;
+import kr.or.dgit.Coffee_Project.service.ProductService;
 import kr.or.dgit.Coffee_Project.Content.ProductContent;
 
 public class CoffeeView extends JFrame {
@@ -53,21 +59,40 @@ public class CoffeeView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(1, 0, 10, 10));
 		setLocationRelativeTo(null);
-		
-		
+
 		coffeecontent = new CoffeeContent();
+		coffeecontent.getPanelPname().setEnable(false);
+		coffeecontent.getPanelPcode().getTextField().addFocusListener(new FocusAdapter() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+								
+				try{
+					 String pCode = coffeecontent.getPanelPcode().getTextField().getText();
+					Product product = ProductService.getInstance().selectItemByno(new Product(pCode, 0));
+					coffeecontent.getPanelPname().setTextValue(product.getpName());
+				}catch(NullPointerException err){
+					JOptionPane.showMessageDialog(null, "등록되지 않은 제품입니다.");
+				}
+				
+				
+			
+			}
+
+		});
+
 		JPanel InputPanel = new JPanel();
 		contentPane.add(InputPanel);
 		InputPanel.setLayout(new BorderLayout(0, 5));
 		InputPanel.add(coffeecontent, BorderLayout.CENTER);
-		
+
 		JPanel InputBtnPanel = new JPanel();
 		InputPanel.add(InputBtnPanel, BorderLayout.SOUTH);
 		InputBtnPanel.setLayout(new GridLayout(0, 3, 5, 0));
-		
+
 		JButton btnAdd = new JButton("\uC785\uB825");
 		btnAdd.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -75,7 +100,7 @@ public class CoffeeView extends JFrame {
 				} catch (Exception e1) {
 					JOptionPane.showMessageDialog(null, "데이터를 모두 입력해주세요.");
 					e1.printStackTrace();
-					//return;
+					// return;
 				}
 				Coffee coffee = coffeecontent.getContent();
 				CoffeeService.getInstance().insertItem(coffee);
@@ -85,104 +110,111 @@ public class CoffeeView extends JFrame {
 			}
 		});
 		InputBtnPanel.add(btnAdd);
-		
+
 		JButton btnOutSmargin = new JButton("\uB9C8\uC9C4\uC561");
 		btnOutSmargin.addActionListener(new ActionListener() {
-			
-			private AbstractListView frame;
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(frame == null){
-					frame = new SmarginView("마진액 순위");
-					frame.loadData();
-					frame.setVisible(true);
-				}else{
-					frame.loadData();
-					frame.setVisible(true);
-				}
-				
-			}
-		});
-		InputBtnPanel.add(btnOutSmargin);
-		
-		JButton btnOutSprice = new JButton("\uD310\uB9E4\uAE08\uC561");
-		btnOutSprice.addActionListener(new ActionListener() {
-			
+
 			private AbstractListView frame;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(frame == null){
+				if (frame == null) {
+					frame = new SmarginView("마진액 순위");
+					frame.loadData();
+					frame.setVisible(true);
+				} else {
+					frame.loadData();
+					frame.setVisible(true);
+				}
+
+			}
+		});
+		InputBtnPanel.add(btnOutSmargin);
+
+		JButton btnOutSprice = new JButton("\uD310\uB9E4\uAE08\uC561");
+		btnOutSprice.addActionListener(new ActionListener() {
+
+			private AbstractListView frame;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (frame == null) {
 					frame = new SpriceView("판매 금액 순위");
 					frame.loadData();
 					frame.setVisible(true);
-				}else{
+				} else {
 					frame.loadData();
 					frame.setVisible(true);
 				}
 			}
 		});
 		InputBtnPanel.add(btnOutSprice);
-		
+
 		JLabel lblInput = new JLabel("\uD310\uB9E4\uC2E4\uC801 \uC785\uB825");
 		lblInput.setFont(new Font("굴림", Font.BOLD, 15));
 		lblInput.setHorizontalAlignment(SwingConstants.CENTER);
 		InputPanel.add(lblInput, BorderLayout.NORTH);
-		
+
 		JPanel ProductPanel = new JPanel();
 		contentPane.add(ProductPanel);
 		ProductPanel.setLayout(new BorderLayout(0, 5));
-		
+
 		listProduct = new ProductListComponent();
 		ProductPanel.add(listProduct, BorderLayout.CENTER);
-		
+
 		JPanel ProductInput = new JPanel();
 		ProductPanel.add(ProductInput, BorderLayout.SOUTH);
 		ProductInput.setLayout(new GridLayout(0, 1, 5, 5));
-		
+
 		ProductContent panel = new ProductContent();
 		ProductInput.add(panel);
-		
+
 		JPanel ProductBtnPanel = new JPanel();
 		ProductInput.add(ProductBtnPanel);
 		ProductBtnPanel.setLayout(new GridLayout(0, 2, 5, 10));
-		
+
 		JButton btnAddProduct = new JButton("\uCD94\uAC00");
+		btnAddProduct.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 		ProductBtnPanel.add(btnAddProduct);
-		
+
 		JButton btnDeleteProduct = new JButton("\uC0AD\uC81C");
 		ProductBtnPanel.add(btnDeleteProduct);
-		
+
 		JLabel lblProduct = new JLabel("\uC81C\uD488\uAD00\uB9AC");
 		lblProduct.setFont(new Font("굴림", Font.BOLD, 15));
 		lblProduct.setHorizontalAlignment(SwingConstants.CENTER);
 		ProductPanel.add(lblProduct, BorderLayout.NORTH);
-		
+
 		JPanel SalePanel = new JPanel();
 		contentPane.add(SalePanel);
 		SalePanel.setLayout(new BorderLayout(0, 5));
-		
+
 		listCoffee = new SaleListComponent();
 		SalePanel.add(listCoffee, BorderLayout.CENTER);
-		
+
 		JPanel SaleBtnPanel = new JPanel();
 		SalePanel.add(SaleBtnPanel, BorderLayout.SOUTH);
 		SaleBtnPanel.setLayout(new GridLayout(1, 0, 5, 0));
-		
+
 		JButton btnDeleteSale = new JButton("\uC0AD\uC81C");
 		btnDeleteSale.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Coffee coffee = listCoffee.getSelectItem();
 				CoffeeService.getInstance().deleteItem(coffee);
 				listCoffee.loadModel();
-				
+
 			}
 		});
 		SaleBtnPanel.add(btnDeleteSale);
-		
+
 		JLabel label = new JLabel("\uC2E4\uC801\uAD00\uB9AC");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("굴림", Font.BOLD, 15));
