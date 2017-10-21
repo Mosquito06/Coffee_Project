0be9ea09a43fbda2fe07205564e468385c389473
Mosquito(H) <skykim10908@naver.com>
2017-10-21 16:12:28 +0900

    DB 배경 작업

--- src/kr/or/dgit/Coffee_Project/CoffeeProjectMain.java
+++ src/kr/or/dgit/Coffee_Project/CoffeeProjectMain.java
@@ -35,8 +35,7 @@ public class CoffeeProjectMain extends JFrame {
 	}
 
 	public CoffeeProjectMain() {
-		
-		
+
 		setTitle("\uBAA8\uAD6C\uC18C\uD504\uD2B8");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 550, 420);
@@ -45,62 +44,61 @@ public class CoffeeProjectMain extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		setLocationRelativeTo(null);
-		
+
 		JPanel BtnPanel = new JPanel();
 		contentPane.add(BtnPanel, BorderLayout.CENTER);
 		BtnPanel.setLayout(null);
-		
+
 		JPanel BtnTopPanel = new JPanel();
 		BtnTopPanel.setBounds(156, 115, 209, 59);
 		BtnPanel.add(BtnTopPanel);
 		BtnTopPanel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JButton btnStart = new JButton("\uC2DC\uC791\uD558\uAE30");
 		btnStart.setFont(new Font("±¼¸²", Font.BOLD, 15));
 		btnStart.addActionListener(new ActionListener() {
-			
+
 			private CoffeeView frame;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(frame == null){
+				if (frame == null) {
 					frame = new CoffeeView();
 					frame.setVisible(true);
-				}else{
+				} else {
 					frame.setVisible(true);
 				}
 			}
 		});
 		BtnTopPanel.add(btnStart);
-		
+
 		JPanel BtnBottomPanel = new JPanel();
 		BtnBottomPanel.setBounds(185, 196, 155, 38);
 		BtnPanel.add(BtnBottomPanel);
 		BtnBottomPanel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JButton BtnData = new JButton("DB \uC124\uCE58\uD558\uAE30");
 		BtnData.addActionListener(new ActionListener() {
-			
+
 			private jdbcSetting frame;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(frame == null){
+				if (frame == null) {
 					frame = new jdbcSetting();
 					frame.setVisible(true);
-				}else{
+				} else {
 					frame.setVisible(true);
 				}
-				
-				
+
 			}
 		});
 		BtnBottomPanel.add(BtnData);
-		
+
 		JLabel ImgLabel = new JLabel("New label");
 		ImgLabel.setIcon(new ImageIcon(System.getProperty("user.dir") + "\\images\\CoffeeBack.jpg"));
 		ImgLabel.setBounds(0, 0, 524, 371);
 		BtnPanel.add(ImgLabel);
-		
+
 	}
 }
